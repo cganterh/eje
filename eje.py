c3be61a04d746aca083c03da0f02815874250f76
Cristóbal Ganter <cganterh@gmail.com>
2021-06-12 22:27:53 -0400

    Added tls support

--- eje.py
+++ eje.py
@@ -1,6 +1,8 @@
 """Tornado and Systemd utilities."""
 
 
+import ssl as _ssl
+
 from logging import getLogger as _get_logger  # noqa: N813
 
 from socket import (  # noqa: N812
@@ -43,7 +45,22 @@ def _listen_to(server: TCPServer, port=None):
             raise
 
 
-def start_server(app: Application, port: Optional[int] = None):
+def _make_ssl_context(certificate_path, private_key_path):
+    if certificate_path is None or private_key_path is None:
+        return None
+
+    else:
+        ssl_context = _ssl.create_default_context(_ssl.Purpose.CLIENT_AUTH)
+        ssl_context.load_cert_chain(certificate_path, private_key_path)
+        return ssl_context
+
+
+def start_server(
+    app: Application,
+    port: Optional[int] = None,
+    certificate_path=None,
+    private_key_path=None,
+):
     """Start a server to serve an app at the specified port.
 
     :param app: A :class:`tornado.web.Application` instance.
@@ -54,7 +71,8 @@ def start_server(app: Application, port: Optional[int] = None):
     :raises GAIError:  # noqa: DAR402
         if there is a problem listening to the specified port.
     """
-    server = _HTTPServer(app)
+    ssl_context = _make_ssl_context(certificate_path, private_key_path)
+    server = _HTTPServer(app, ssl_options=ssl_context)
     _listen_to(server, port)
 
     ports = [s.getsockname()[1] for s in server._sockets.values()]

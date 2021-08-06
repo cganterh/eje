5bbc78fc12f1b274cac6b44d85999693eb77a400
Cristóbal Ganter <cganterh@gmail.com>
2021-08-05 22:14:04 -0400

    Relaxed tornado version specifier

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup  # type: ignore
 setup(
     author_email="cganterh@gmail.com",
     author="Cristóbal Ganter",
-    install_requires=["setuptools>=42.0", "tornado~=6.1"],
+    install_requires=["setuptools>=42.0", "tornado~=6.0"],
     name="eje",
     py_modules=["eje"],
     setup_requires=["setuptools_scm"],
